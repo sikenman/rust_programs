9effc95d84c4f0dc870c650e1b9c50547a14e0e1
Siken Man Dongol <sikenman@yahoo.com>
2023-04-07 02:08:39 -0500

    Update validate_us_phone.rs

--- validate_us_phone.rs
+++ validate_us_phone.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
 
 fn is_valid_us_phone_no(phone_number: &str) -> bool {
+    
     let regex = Regex::new(r#"^\(?[2-9][0-8][0-9]\)?[-.\s]?[2-9][0-9]{2}-[0-9]{4}$"#).unwrap();
     
     // validates US Phone Numbers of the following two formats
@@ -12,7 +13,6 @@ fn is_valid_us_phone_no(phone_number: &str) -> bool {
 
 fn main() {
     let phone_numbers = ["(555) 555-1234", "555-555-1234", "1-555-555-1234", "5555551234"];
-
 
     for ph in phone_numbers {
         println!("{} is valid: {}", ph, is_valid_us_phone_no(ph));
