475e186e4d417abe03e20b725345f6d78048b0d1
Siken Man Dongol <sikenman@yahoo.com>
2023-04-07 01:31:57 -0500

    Prints ASCII characters
    The following Rust program showcases the usage of the loop keyword along with break and continue. It prints all ASCII characters ranging from space (32) to the end of the ASCII table (255), while ignoring unprintable characters between 127 to 160.

---
+++ ascii_characters.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let mut ascii_code = 32;    // 32 is for space
+    let mut line_break = 0;
+    
+    loop {
+        let ascii_char = ascii_code as u8 as char;
+
+        // bypassing characters that are not printable
+        if ascii_code > 126 && ascii_code <= 160 {
+            ascii_code += 1;
+            continue;
+        }
+        
+        print!("{} ", ascii_char);
+        ascii_code += 1;
+        
+        // break line after printing 15 characters
+        if line_break==15 {
+            println!();
+            line_break = 0;
+        } else {
+            line_break += 1;
+        }
+        // go out of loop once we reach end of ASCII
+        if ascii_code == 256 {
+            break;
+        }
+
+    }
+}
