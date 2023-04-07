eb9f5d61ae842acdeb15be57f1beb74e1bda3bac
Siken Man Dongol <sikenman@yahoo.com>
2023-04-07 00:25:48 -0500

    PowerBall 10 generator

---
+++ powerball.rs
@@ -0,0 +1,22 @@
+use rand::{seq::SliceRandom};
+
+fn main() {
+
+    let mut rng = rand::thread_rng();
+    
+    for _ in 1..=10 {
+        let white_balls: Vec<u8> = (1..70).collect();
+        let _power_ball: Vec<u8> = (1..27).collect();
+        
+        let mut selected_white_balls: Vec<u8> = white_balls
+            .choose_multiple(&mut rng, 5)
+            .copied()
+            .collect();
+        
+        selected_white_balls.sort();
+        
+        let selected_power_ball = *_power_ball.choose(&mut rng).unwrap();
+        
+        println!("WBs {:?}, PB ({})", selected_white_balls, selected_power_ball);
+    }
+}
