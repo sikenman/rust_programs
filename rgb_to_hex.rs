7152f4a64df31963c5e317067006ce972ec62eb8
Siken Man Dongol <sikenman@yahoo.com>
2023-04-07 12:24:50 -0500

    RGB(X,X,X) to hex format
    converts RGB(X,X,X) into hexadecimal color code used in CSS

---
+++ rgb_to_hex.rs
@@ -0,0 +1,32 @@
+use std::io::{self, Write};
+
+fn main() {
+    let mut input = String::new();
+    print!("Enter an RGB value in the format (X,X,X): ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+
+    // Parse the input string into separate RGB components
+    let components: Vec<&str> = input.trim().trim_matches(|c| c == '(' || c == ')')
+                                       .split(',')
+                                       .collect();
+    if components.len() != 3 {
+        panic!("Invalid input: expected 3 comma-separated values");
+    }
+    println!("{:?}", components);
+
+    // Parse the RGB components as integers and validate their range
+    let mut rgb = [0u8; 3];
+    for (i, component) in components.iter().enumerate() {
+        let value = component.trim().parse::<u8>().unwrap();
+        if value < 1 || value > 255 {
+            panic!("Invalid input: RGB component {} is out of range", i+1);
+        }
+        rgb[i] = value;
+    }
+
+    // Convert the RGB value to a hexadecimal string
+    let hex = format!("#{:X}{:X}{:X}", rgb[0], rgb[1], rgb[2]);
+
+    println!("The rgb{} => {}", input.trim(), hex);
+}
