fc3b89197e6efcb2b7db75c3bae952a44eab16d9
Siken Man Dongol <sikenman@yahoo.com>
2023-04-07 02:07:29 -0500

    Validates us phone nos
    Validates US Phone Numbers of the following two formats:
    1. (XXX) XXX-XXXX -> (555) 555-1234
    2. XXX-XXX-XXXX   -> 555-555-2345

--- validate_us_phone.rs
+++ validate_us_phone.rs
@@ -2,7 +2,11 @@ use regex::Regex;
 
 fn is_valid_us_phone_no(phone_number: &str) -> bool {
     let regex = Regex::new(r#"^\(?[2-9][0-8][0-9]\)?[-.\s]?[2-9][0-9]{2}-[0-9]{4}$"#).unwrap();
-
+    
+    // validates US Phone Numbers of the following two formats
+    // (XXX) XXX-XXXX -> (555) 555-1234
+    // XXX-XXX-XXXX   -> 555-555-2345
+    
     regex.is_match(phone_number)
 }
 
