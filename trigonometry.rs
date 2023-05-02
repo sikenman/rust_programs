a1ecdae9130dfc85f7ea34938289c96ce367c44d
Siken Man Dongol <sikenman@yahoo.com>
2023-05-02 08:08:43 -0500

    Update trigonometry.rs

--- trigonometry.rs
+++ trigonometry.rs
@@ -1,3 +1,8 @@
+/*
+This program uses the `sin()`, `cos()`, `tan()`, and `ln()` functions from the Rust standard library 
+to calculate the sine, cosine, tangent, and natural logarithm of some numbers. 
+*/
+
 use std::f64::consts::PI;
 
 fn main() {
@@ -11,3 +16,13 @@ fn main() {
     println!("log({}) = {}", x * y * z, (x * y * z).ln());
     println!("pi = {}", PI);
 }
+
+/* Output of the program
+
+sin(1.5707963267948966) = 1
+cos(1.5707963267948966) = 0.00000000000000006123233995736766
+tan(1.0471975511965976) = 1.7320508075688767
+log(2.5838563900249847) = 0.9492830077602001
+pi = 3.141592653589793
+
+*/
